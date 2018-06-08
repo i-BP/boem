c381b90b952f65e785d8b8d8494a6e621d4d1886
Jacob Geisel <jacob.geisel@gmail.com>
2018-06-08 11:34:13 +0200

    moving some env variables to clear text
    and disabling temporarily tests

--- pushToBintray.sh
+++ pushToBintray.sh
@@ -17,13 +17,15 @@ deploy_updatesite
 }
 
 function deploy_updatesite() {
-echo "${BINTRAY_USER}"
-echo "${BINTRAY_API_KEY}"
-echo "${BINTRAY_OWNER}"
-echo "${BINTRAY_REPO}"
-echo "${PCK_NAME}"
-echo "${PCK_VERSION}"
-echo "${PATH_TO_REPOSITORY}"
+echo "==Infos"
+echo "BINTRAY_USER=${BINTRAY_USER}"
+echo "BINTRAY_API_KEY=${BINTRAY_API_KEY}"
+echo "BINTRAY_OWNER=${BINTRAY_OWNER}"
+echo "BINTRAY_REPO=${BINTRAY_REPO}"
+echo "PCK_NAME=${PCK_NAME}"
+echo "PCK_VERSION=${PCK_VERSION}"
+echo "PATH_TO_REPOSITORY=${PATH_TO_REPOSITORY}"
+echo "==End Infos"
 
 if [ ! -z "$PATH_TO_REPOSITORY" ]; then
    cd $PATH_TO_REPOSITORY
